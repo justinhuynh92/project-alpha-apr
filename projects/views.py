dce85764547e76b65b04d45ae0430e316c6855bf
Justin Huynh <justinhuynh639@gmail.com>
2023-06-05 16:48:07 -0700

    Feature 7 complete

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from projects.models import Project
 
 # Create your views here.
--- tracker/urls.py
+++ tracker/urls.py
@@ -26,5 +26,6 @@ def redirect_to_home(request):
 urlpatterns = [
     path("", redirect_to_home),
     path("admin/", admin.site.urls),
-    path("projects/", include("projects.urls"))
+    path("projects/", include("projects.urls")),
+    path("accounts/", include("accounts.urls"))
 ]
